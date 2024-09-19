8e44396e84cb4c2392bcd392a1fd8a30de4022e0
DragonautMk <michelesavarese1997@gmail.com>
2024-09-19 09:24:49 +0200

    esercizio 4

---
+++ 4.js
@@ -0,0 +1,7 @@
+let persona = {
+'nome': 'Marco',
+'cognome': 'Lucarelli',
+'età':'27',
+}
+
+console.log(`Ciao sono ${persona.nome} ${persona.cognome} e ho ${persona.età} e partecipo a Temptetation Island con la mia amic- no volevo dire findanzata per metterci alla prova.`);
